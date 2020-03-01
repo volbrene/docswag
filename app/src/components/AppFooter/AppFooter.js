af85b4be121f9123c17bdb824a7a6af6a1f47a9a
Rene Volbach <rene.volbach@un-iq.de>
2020-03-01 20:43:02 +0100

    update footer

--- app/src/components/AppFooter/AppFooter.js
+++ app/src/components/AppFooter/AppFooter.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react';
-import './AppFooter.css';
+import React, { Component } from "react";
+import "./AppFooter.css";
 
 class AppFooter extends Component {
+  componentDidMount() {
+    const script = document.createElement("script");
+
+    script.src = "https://buttons.github.io/buttons.js";
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
       <footer>
-          <a className="copyright" href="https://hub.docker.com/r/volbrene/redoc">created by volbrene</a>
+        <div className="inner">
+          <a
+            class="github-button"
+            href="https://github.com/volbrene"
+            aria-label="Follow @volbrene on GitHub"
+          >
+            Follow @volbrene
+          </a>
+          <a
+            class="github-button"
+            href="https://github.com/volbrene/docswag"
+            data-icon="octicon-star"
+            aria-label="Star volbrene/docswag on GitHub"
+          >
+            Star
+          </a>
+        </div>
       </footer>
     );
   }
